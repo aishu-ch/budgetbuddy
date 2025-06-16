98181208fb04e9d34eb966c9ff26f80893735b51
aishu-ch <aishu.chinnaiya1112@gmail.com>
2025-06-16 22:02:15 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/layout/Layout';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Dashboard } from './pages/Dashboard';
+import { Transactions } from './pages/Transactions';
+export function App() {
+  return <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>;
+}
